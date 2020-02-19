224b7ca2cb0a323cd857629a2c331fc9aedc8e7b
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 02:48:02 +0300

    Update RemoveGreaterKeyCommand.java

--- src/main/java/ru/students/lab/commands/collectionhandlers/RemoveGreaterKeyCommand.java
+++ src/main/java/ru/students/lab/commands/collectionhandlers/RemoveGreaterKeyCommand.java
@@ -3,12 +3,20 @@ package ru.students.lab.commands.collectionhandlers;
 import ru.students.lab.client.IHandlerInput;
 import ru.students.lab.commands.ICommand;
 import ru.students.lab.managers.CollectionManager;
+ /** 
+ * Класс для выполнения и получения информации о функции удаления из коллекции элементов, ключ которых превышает заданный
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 
 public class RemoveGreaterKeyCommand implements ICommand {
 
     public static final String DESCRIPTION = "удалить из коллекции все элементы, ключ которых превышает заданный\nSyntax: remove_greater_key key";
     private CollectionManager collectionManager;
-
+    /** 
+     * Конструктор - создает объект класса RemoveGreaterKeyCommand и экземпляр класса collectionManager для последущей работе с коллекцией
+     * @see RemoveGreaterKeyCommand#RemoveGreaterKeyCommand(CollectionManager)
+     */
     public RemoveGreaterKeyCommand(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
     }

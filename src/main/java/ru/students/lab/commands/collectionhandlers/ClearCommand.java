30722de0a50a6e23c7253a5f510eed5b4dd2f997
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 02:00:44 +0300

    Update ClearCommand.java

--- src/main/java/ru/students/lab/commands/collectionhandlers/ClearCommand.java
+++ src/main/java/ru/students/lab/commands/collectionhandlers/ClearCommand.java
@@ -5,10 +5,18 @@ import ru.students.lab.commands.ICommand;
 import ru.students.lab.managers.CollectionManager;
 
 public class ClearCommand implements ICommand {
+ /** 
+ * Класс для выполнения и получения информации о функции очистки коллекции
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 
     public static final String DESCRIPTION = "очистить коллекцию";
     private CollectionManager collectionManager;
-
+    /** 
+     * Конструктор - создает объект класса clearCommand и экземпляр класса collectionManager для последущей работе с коллекцией
+     * @see ClearCommand#ClearCommand(CollectionManager)
+     */
     public ClearCommand(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
     }

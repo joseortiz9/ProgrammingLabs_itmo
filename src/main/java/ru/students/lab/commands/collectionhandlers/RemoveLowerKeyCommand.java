7fa9c6bd79282c8f6cd6150093532943a4975ad0
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 02:50:47 +0300

    Update RemoveLowerKeyCommand.java

--- src/main/java/ru/students/lab/commands/collectionhandlers/RemoveLowerKeyCommand.java
+++ src/main/java/ru/students/lab/commands/collectionhandlers/RemoveLowerKeyCommand.java
@@ -3,11 +3,19 @@ package ru.students.lab.commands.collectionhandlers;
 import ru.students.lab.client.IHandlerInput;
 import ru.students.lab.commands.ICommand;
 import ru.students.lab.managers.CollectionManager;
-
+ /** 
+ * Класс для выполнения и получения информации о функции удаления из коллекции элементов, ключ которых меньше чем заданный
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 public class RemoveLowerKeyCommand implements ICommand {
 
     public static final String DESCRIPTION = "удалить из коллекции все элементы, ключ которых меньше, чем заданный.\nSyntax: remove_lower_key key";
     private CollectionManager collectionManager;
+     /** 
+     * Конструктор - создает объект класса RemoveLowerKeyCommand и экземпляр класса collectionManager для последущей работе с коллекцией
+     * @see RemoveLowerKeyCommand#RemoveLowerKeyCommand(CollectionManager)
+     */
 
     public RemoveLowerKeyCommand(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;

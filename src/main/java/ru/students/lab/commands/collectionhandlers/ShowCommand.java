49157423ebdb5dc1ab49cb7ae467d151bb73817c
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 02:57:17 +0300

    Update ShowCommand.java

--- src/main/java/ru/students/lab/commands/collectionhandlers/ShowCommand.java
+++ src/main/java/ru/students/lab/commands/collectionhandlers/ShowCommand.java
@@ -3,12 +3,21 @@ package ru.students.lab.commands.collectionhandlers;
 import ru.students.lab.client.IHandlerInput;
 import ru.students.lab.commands.ICommand;
 import ru.students.lab.managers.CollectionManager;
+ /** 
+ * Класс для выполнения и получения информации о функции вывода всех элементов коллекции в строковом представлении
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 
 public class ShowCommand implements ICommand {
+    
 
     public static final String DESCRIPTION = "вывести в стандартный поток вывода все элементы коллекции в строковом представлении";
     private CollectionManager collectionManager;
-
+    /** 
+     * Конструктор - создает объект класса ShowCommand и экземпляр класса collectionManager для последующей работы с коллекцией
+     * @see ShowCommand#ShowCommand(CollectionManager)
+     */
     public ShowCommand(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
     }

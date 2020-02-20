9f8af08ed5d12065ed53454d5aacac541ec59f55
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 03:33:24 +0300

    Update EmptyFileException.java

--- src/main/java/ru/students/lab/exceptions/EmptyFileException.java
+++ src/main/java/ru/students/lab/exceptions/EmptyFileException.java
@@ -1,8 +1,17 @@
 package ru.students.lab.exceptions;
 
 import java.io.IOException;
-
+ /** 
+ * Класс для исключений связанных с отсутствием данных в файле
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 public class EmptyFileException extends IOException {
+     /** 
+     * Конструктор - создает объект класса EmptyFileException
+     * @param s - сообщение об исключении
+     * @see EmptyFileException#EmptyFileException(s)
+     */
     public EmptyFileException(String s) {
         super(s);
     }

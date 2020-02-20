c9340415d2e7d53b9f97fd640c77dd60c4d838df
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 03:36:07 +0300

    Update NoSuchCommandException.java

--- src/main/java/ru/students/lab/exceptions/NoSuchCommandException.java
+++ src/main/java/ru/students/lab/exceptions/NoSuchCommandException.java
@@ -1,6 +1,16 @@
 package ru.students.lab.exceptions;
+ /** 
+ * Класс для исключений связанных с отсутствием вводимой команды
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 
 public class NoSuchCommandException extends RuntimeException {
+     /** 
+     * Конструктор - создает объект класса NoSuchCommandException
+     * @param s - сообщение об исключении
+     * @see NoSuchCommandException#NoSuchCommandException(s)
+     */
     public NoSuchCommandException(String s) {
         super(s);
     }

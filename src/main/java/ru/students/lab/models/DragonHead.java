52ca86f29ab942525ffb5a77e9ccdcf5bd162186
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 03:31:04 +0300

    Update DragonHead.java

--- src/main/java/ru/students/lab/models/DragonHead.java
+++ src/main/java/ru/students/lab/models/DragonHead.java
@@ -1,11 +1,23 @@
 package ru.students.lab.models;
+ /** 
+ * Класс модели головы дракона
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 
 public class DragonHead {
     private Double eyesCount; //Поле может быть null
-
+    /** 
+     * Конструктор - создает пустой объект класса DragonHead
+     * @see DragonHead#DragonHead()
+     */
     public DragonHead() {
     }
-
+    /** 
+     * Конструктор - создает непустой объект класса DragonHead
+     * @param eyesCount - количество глаз у дракона
+     * @see DragonHead#DragonHead(eyesCount)
+     */
     public DragonHead(Double eyesCount) {
         this.eyesCount = eyesCount;
     }

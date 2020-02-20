4ca1a6fd3805c3ec960082a2ff20dbd0da9689e2
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 04:02:56 +0300

    Update FileManager.java

--- src/main/java/ru/students/lab/managers/FileManager.java
+++ src/main/java/ru/students/lab/managers/FileManager.java
@@ -9,12 +9,21 @@ import ru.students.lab.models.Dragon;
 
 import java.io.*;
 import java.util.HashMap;
-
+ /** 
+ * Класс для работы с файлами
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 public class FileManager {
 
     private XStream xmlParser;
     private File xmlDragons;
-
+    /** 
+     * Конструктор - создает объект класса FileManager
+     * @param dataFilePath - строка, содержащая путь до файла с данными
+     * @throws FileNotFoundException В случае если файл нельзя найти.
+     * @see FileManager#FileManager(dataFilePath)
+     */
     public FileManager(String dataFilePath) throws FileNotFoundException {
         if (dataFilePath == null || !(new File(dataFilePath).exists()))
             throw new FileNotFoundException("There is not such file!");
@@ -25,7 +34,10 @@ public class FileManager {
         xmlParser.alias("root", java.util.Map.class);
         xmlParser.alias("dragon", Dragon.class);
     }
-
+    /**
+     * Функция сохранения коллекции в формате xml в файл
+     * @param collection - Хэшмэп, содержащий коллекцию экземпляров класса Dragon
+     */
 
     public void SaveCollectionInXML(HashMap<Integer, Dragon> collection) throws XStreamException, IOException {
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.getXmlDragons())))) {
@@ -33,7 +45,10 @@ public class FileManager {
             writer.write(xml);
         }
     }
-
+    /**
+     * Функция получения коллекции из файла
+     * @return - возвращает collection - Хэшмэп, содержащий коллекцию экземпляров класса Dragon
+     */
 
     public HashMap<Integer, Dragon> getCollectionFromFile() throws XStreamException, IOException {
         HashMap<Integer, Dragon> collection = new HashMap<Integer, Dragon>();
@@ -44,6 +59,11 @@ public class FileManager {
 
         return collection;
     }
+    /**
+     * Функция получения строки из файла
+     * @param filePath - путь к файлу с данными
+     * @return - возвращает dataStr - строку с данными
+     */
 
     public String getStrFromFile(String filePath) throws IOException {
         File fileToRetrieve;

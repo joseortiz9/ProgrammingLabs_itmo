4f2c9760f6b30917fa60e8c0c76768f8371883cf
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 03:11:52 +0300

    Update ManDescriptorCommand.java

--- src/main/java/ru/students/lab/commands/ManDescriptorCommand.java
+++ src/main/java/ru/students/lab/commands/ManDescriptorCommand.java
@@ -3,12 +3,19 @@ package ru.students.lab.commands;
 import ru.students.lab.client.IHandlerInput;
 
 import java.util.Map;
-
+ /** 
+ * Класс для описания команды по ее ключу
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
 public class ManDescriptorCommand implements ICommand {
 
     public static final String DESCRIPTION = "Describe a command by its key";
     private Map<String, ICommand> commandsDictionary;
-
+    /** 
+     * Конструктор - создает объект класса ManDescriptorCommandи экземпляр класса commandsDictionary для вывода информации о команде по ее ключу
+     * @see ManDescriptorCommand#ManDescriptorCommand(commandsDictionary)
+     */
     public ManDescriptorCommand(Map<String, ICommand> commandsDictionary) {
         this.commandsDictionary = commandsDictionary;
     }

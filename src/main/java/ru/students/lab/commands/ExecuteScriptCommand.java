13cd62068882b8443c361471028595b67e8f39a8
Neyylar <61251909+Neyylar@users.noreply.github.com>
2020-02-20 03:08:25 +0300

    Update ExecuteScriptCommand.java

--- src/main/java/ru/students/lab/commands/ExecuteScriptCommand.java
+++ src/main/java/ru/students/lab/commands/ExecuteScriptCommand.java
@@ -6,12 +6,21 @@ import ru.students.lab.managers.CommandManager;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Map;
+ /** 
+ * Класс для выполнения и получения информации о функции считывания скрипта из указанного файла
+ * @autor Хосе Ортис
+ * @version 1.0
+*/
+
 
 public class ExecuteScriptCommand implements ICommand {
 
     public static final String DESCRIPTION = "считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.\nSyntax: execute_script file_name";
     private CommandManager commandManager;
-
+    /** 
+     * Конструктор - создает объект класса ExecuteScriptCommand и экземпляр класса commandManager для последущего выполнения команд
+     * @see ExecuteScriptCommand#ExecuteScriptCommand(CollectionManager)
+     */
     public ExecuteScriptCommand(CommandManager commandManager) {
         this.commandManager = commandManager;
     }
